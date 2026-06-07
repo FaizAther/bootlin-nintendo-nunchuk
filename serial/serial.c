313eedbe51c2640a43c5387956011f5a951aad41
Faiz Ather <faizather@protonmail.com>
2026-06-07 13:21:35 +1000

    DMA compilation

--- serial/serial.c
+++ serial/serial.c
@@ -12,6 +12,11 @@
 #include <linux/interrupt.h>
 #include <linux/wait.h>
 #include <linux/poll.h>
+#include <linux/dmaengine.h>
+#include <linux/dma-mapping.h>
+
+#define OMAP_UART_SCR_DMAMODE_CTL3 0x7
+#define OMAP_UART_SCR_TX_TRIG_GRANU1 BIT(6)
 
 
 #define SERIAL_RESET_COUNTER 0
@@ -28,8 +33,15 @@ struct serial_dev {
         char rx_buf[SERIAL_BUFSIZE];
         unsigned int buf_rd;
         unsigned int buf_wr;
-        wait_queue_head_t wait;
         spinlock_t serial_lock;
+        char tx_buf[SERIAL_BUFSIZE];
+        unsigned int buf_tx_rd;
+        unsigned int buf_tx_wr;
+        wait_queue_head_t wait;
+        struct dma_chan *txchan;
+        dma_addr_t dma_map_resource;
+        dma_addr_t fifo_dma_addr;
+        struct device *dev;
 };
 
 
@@ -83,7 +95,7 @@ static long serial_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 }
 
 // Write file op allows user space to send characters via echo/write
-static ssize_t serial_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
+static ssize_t serial_write_pio(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
 {
         struct serial_dev *serial = file->private_data;
         char kbuf[64]; // Temporary safe kernel buffer
@@ -119,7 +131,7 @@ static int serial_open(struct inode *inode, struct file *file)
         return 0;
 }
 
-static ssize_t serial_read(struct file *file, char __user *user_buffer, size_t count, loff_t *offset)
+static ssize_t serial_read_pio(struct file *file, char __user *user_buffer, size_t count, loff_t *offset)
 {
         struct serial_dev *serial = file->private_data;
         size_t bytes_read = 0;
@@ -170,14 +182,40 @@ static __poll_t serial_poll(struct file *file, struct poll_table_struct *wait)
         return mask;
 }
 
-static const struct file_operations serial_fops = {
-        .owner          = THIS_MODULE,
-        .open           = serial_open,
-        .release        = serial_close,
-        .write          = serial_write,
-        .read           = serial_read,
-        .poll           = serial_poll,
-        .unlocked_ioctl = serial_ioctl,
+static ssize_t serial_write_dma(struct file *file, const char __user *user_buffer, size_t count, loff_t *offset)
+{
+        return 0;
+}
+
+void serial_cleanup_dma(struct platform_device *pdev, struct serial_dev *serial)
+{
+        int err = dmaengine_terminate_sync(serial->txchan);
+        if (err) {
+                dev_err(serial->dev, "Failed to terminate DMA: %d\n", err);
+        }
+        dma_unmap_resource(serial->dev, serial->fifo_dma_addr, 4, DMA_TO_DEVICE, 0);
+        dma_release_channel(serial->txchan);
+        return;
+}
+
+static struct file_operations serial_fops_pio = {
+    .owner   = THIS_MODULE,
+    .open    = serial_open,
+    .read    = serial_read_pio,
+    .write   = serial_write_pio,
+    .unlocked_ioctl   = serial_ioctl,
+    .poll    = serial_poll,
+    .release = serial_close,
+};
+
+static struct file_operations serial_fops_dma = {
+    .owner   = THIS_MODULE,
+    .open    = serial_open,
+    .read    = serial_read_pio,
+    .write   = serial_write_dma,
+    .unlocked_ioctl   = serial_ioctl,
+    .poll    = serial_poll,
+    .release = serial_close,
 };
 
 const struct of_device_id bootlin_of_match[] = {
@@ -214,6 +252,32 @@ static irqreturn_t serial_irq_handler(int irq, void *dev_id)
         return IRQ_HANDLED;
 }
 
+int serial_init_dma(struct platform_device *pdev, struct serial_dev *serial)
+{
+
+        serial->miscdev.fops = &serial_fops_dma;
+        struct dma_slave_config txconf = {};
+        struct resource *res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+        if (!res) {
+                return -ENOMEM;
+        }
+        serial->fifo_dma_addr = dma_map_resource(serial->dev, res->start + UART_TX * 4, 4, DMA_TO_DEVICE, 0);
+        if (dma_mapping_error(serial->dev, serial->fifo_dma_addr)) {
+                dev_err(serial->dev, "Failed to map resource: %pad\n", &serial->fifo_dma_addr);
+                return -ENOMEM;
+        }
+        txconf.direction = DMA_MEM_TO_DEV;
+        txconf.dst_addr_width = DMA_SLAVE_BUSWIDTH_1_BYTE;
+        txconf.dst_addr = serial->fifo_dma_addr;
+        int ret = dmaengine_slave_config(serial->txchan, &txconf);
+        if (ret) {
+                dev_err(serial->dev, "Failed to configure DMA: %d\n", ret);
+                return ret;
+        }
+        reg_write(serial, OMAP_UART_SCR_DMAMODE_CTL3 | OMAP_UART_SCR_TX_TRIG_GRANU1, UART_OMAP_SCR);
+        return 0;
+}
+
 static int serial_probe(struct platform_device *pdev)
 {
         struct serial_dev *serial;
@@ -230,6 +294,7 @@ static int serial_probe(struct platform_device *pdev)
         pr_info("platform_device->dev.driver_data: %p\n", &pdev->dev.driver_data);
         pr_info("platform_device->dev.driver_data: %p\n", pdev->dev.driver_data);
         serial = devm_kzalloc(&pdev->dev, sizeof(struct serial_dev), GFP_KERNEL);
+        serial->dev = &pdev->dev;
         if (!serial) {
                 pr_err("Failed to allocate memory for serial_dev\n");
                 return -ENOMEM;
@@ -276,7 +341,6 @@ static int serial_probe(struct platform_device *pdev)
                 return -ENOMEM;
         }
 
-        serial->miscdev.fops = &serial_fops;
         serial->miscdev.parent = &pdev->dev;  // Tie node lifecycle to device tree entry
 
         ret = misc_register(&serial->miscdev);
@@ -309,6 +373,15 @@ static int serial_probe(struct platform_device *pdev)
         serial->buf_rd = serial->buf_wr = 0;
 
         spin_lock_init(&serial->serial_lock);
+
+        serial->txchan = dma_request_chan(&pdev->dev, pdev->name);
+        if (IS_ERR(serial->txchan)) {
+                ret = PTR_ERR(serial->txchan);
+                dev_err(&pdev->dev, "Failed to request TX channel: %pe\n", serial->txchan);
+                serial->miscdev.fops = &serial_fops_pio;
+        } else {
+                serial_init_dma(pdev, serial);
+        }
 
         dev_info(&pdev->dev, "Misc device registered successfully\n");
         return 0;
